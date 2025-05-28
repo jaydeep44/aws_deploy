8b2994a87b431bd26a0c038eac26f689df13b5d8
Gourav01dev <gourava0556@gmail.com>
2025-05-28 17:32:15 +0530

    add eslint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 const app = express();
 
+console.log("hello");
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
